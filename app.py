7b427b3fd0e103b74c9f8566da3705b349f17062
j396mats <j520396b@mails.cc.ehime-u.ac.jp>
2024-11-28 13:40:44 +0900

    a

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from value import process_text  # value.pyから関数をインポート
 
-def highlight_text(text, ranges, color):
+def highlight_text(text, ranges):
     highlighted = ""
     last_idx = 0
     
-    for start_idx, end_idx in ranges:
+    for start_idx, end_idx,score in ranges:
         # start_idx, end_idxがテキストの範囲内であるかを確認
         if start_idx < 0 or end_idx >= len(text) or start_idx >= end_idx:
             continue  # 範囲外の場合はスキップ
 
+        color = get_highlight_color(score)
         # ハイライト処理
         highlighted += text[last_idx:start_idx]
         highlighted += f'<span style="background-color: {color};">{text[start_idx:end_idx]}</span>'
@@ -24,12 +25,11 @@ def get_highlight_color(score):
     """
     スコアに基づいてハイライト色を決定する関数
     """
-    if score >= 80:
-        return "red"
-    elif score >= 50:
-        return "yellow"
+    if score >= 0:
+        return f'rgb({255},{255 - min(score * 8 * 255,255)},{255 - min(score * 8 * 255,255)})' #AI度を赤く描写
     else:
-        return "green"
+        score *= -1
+        return f'rgb({255 - min(score * 8 * 255,255)},{255 - min(score * 8 * 255,255)},{255})' #人間度を青く描写
 
 st.title("真贋判定アプリ")
 
@@ -42,16 +42,20 @@ else:
 if st.button("判定"):
     if text:
         # テキストをvalues.pyに渡して処理し、複数の範囲とスコアを受け取る
-        highlight_ranges, score = process_text(text)
+        pred_label, total_score, highlight_ranges = process_text(text)
 
-        # スコアに基づいてハイライト色を決定
-        highlight_color = get_highlight_color(score)
-
-        # 複数のハイライト処理を行う
-        highlighted_text = highlight_text(text, highlight_ranges, highlight_color)
+        # 複数のハイライト処理を行
+        highlighted_text = highlight_text(text, highlight_ranges)
         st.markdown(highlighted_text, unsafe_allow_html=True)
         
         # スコアを一度だけ表示（パーセント表示）
-        st.write(f"AI度: {score:.2f}%")
+        if pred_label!=1:
+            st.write(f"結果：人間")
+            st.write(f"人間度: {1 - total_score:.2f}%")
+        else:
+            st.write(f"結果：AI")
+            st.write(f"AI度: {total_score:.2f}%")
+        
     else:
         st.warning("テキストを入力してください。")
+        
--- value.py
+++ value.py
@@ -1,5 +1,7 @@
 def process_text(text):
-    highlight_ranges = [(10, 20), (30, 40)]  # ハイライト範囲
-    score = 75.0  # 仮のスコア
-    
-    return highlight_ranges, score
+    #仮作成
+    highlight_ranges = [(10, 20,0.03), (30, 40,0.113)]  # ハイライト範囲,スコア
+    total_score = 60.0 #仮のAI度
+    pred_label = 1 #仮ラベル 
+
+    return pred_label,total_score,highlight_ranges
